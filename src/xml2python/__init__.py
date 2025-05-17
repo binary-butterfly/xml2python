cc9ec12db22be885ed7ed241aa6c163588dd1e8b
Ernesto Ruge <ernesto.ruge@binary-butterfly.de>
2025-05-17 08:52:33 +0200

    dynamic versioning

--- src/xml2python/__init__.py
+++ src/xml2python/__init__.py
@@ -4,3 +4,10 @@ Use of this source code is governed by an MIT-style license that can be found in
 """
 
 from .xml2python import Xml2Python
+
+try:
+    from ._version import version
+
+    __version__ = version
+except ImportError:
+    __version__ = 'dev'
